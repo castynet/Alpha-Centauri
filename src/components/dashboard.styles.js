69213e9c5d49953cb94bc03fe7150b4e66bad2a2
castynet <riungemaina@gmail.com>
2021-08-23 16:05:50 +0300

    components/dashboard.styles: created and added basic styled

---
+++ src/components/dashboard.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Sidebar = styled.div`
+  width: 25%;
+  background-color: lightblue;
+  height: 100vh;
+`;
+
+export const Main = styled.div`
+  width: 75%;
+  background-color: lightcoral;
+  min-height: 100vh;
+`;
