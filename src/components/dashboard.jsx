ae4433c50797cdaa0c277d663048fdf2f2519766
castynet <riungemaina@gmail.com>
2021-08-23 16:05:55 +0300

    components/dashboard: created

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import * as styled from "./dashboard.styles";
+
+export default function DashBoard() {
+  return (
+    <>
+      <styled.Wrapper>
+        <styled.Sidebar></styled.Sidebar>
+        <styled.Main></styled.Main>
+      </styled.Wrapper>
+    </>
+  );
+}
