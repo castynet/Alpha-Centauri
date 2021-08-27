ee8d7e3778f7edb435a7f8da0091429ca9824334
castynet <riungemaina@gmail.com>
2021-08-27 23:04:34 +0300

    components/user.styles: created & added styels

---
+++ src/components/user.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  right: 50px;
+  top: 30px;
+`;
+
+export const ProfileIcon = styled.div`
+  height: ${(props) => (props.openUser ? "350px" : "60px")};
+  width: ${(props) => (props.openUser ? "350px" : "60px")};
+  border-radius: ${(props) => (props.openUser ? "var(--borderRadius)" : "50%")};
+  border: 2px solid lightblue;
+  transition: var(--smoothTransition);
+  box-shadow: var(--levelOneDropShadow);
+
+  :hover {
+    cursor: pointer;
+  }
+`;
