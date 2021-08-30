1365af217d3998ca06eef95962abe8551c618b98
castynet <riungemaina@gmail.com>
2021-08-30 11:07:36 +0300

    components/dashboard: added menu items

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,7 +4,18 @@ export default function DashBoard() {
   return (
     <>
       <styled.Wrapper>
-        <styled.Sidebar></styled.Sidebar>
+        <styled.Sidebar>
+          <styled.Menu>
+            <styled.MenuItem>Courses</styled.MenuItem>
+            <styled.MenuItem>Assignements</styled.MenuItem>
+            <styled.MenuItem>Tests</styled.MenuItem>
+            <styled.MenuItem>Community</styled.MenuItem>
+          </styled.Menu>
+          <styled.BrowseMenu>
+            <styled.BrowseMenuItem>Messages</styled.BrowseMenuItem>
+            <styled.BrowseMenuItem>All Courses</styled.BrowseMenuItem>
+          </styled.BrowseMenu>
+        </styled.Sidebar>
         <styled.Main></styled.Main>
       </styled.Wrapper>
     </>
