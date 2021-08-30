dd220d0970234da836161e56e7498689c965bf25
castynet <riungemaina@gmail.com>
2021-08-30 11:07:54 +0300

    components/dashboard.styles: styled sidebar menu

--- src/components/dashboard.styles.js
+++ src/components/dashboard.styles.js
@@ -7,13 +7,41 @@ export const Wrapper = styled.div`
 `;
 
 export const Sidebar = styled.div`
-  width: 25%;
+  width: 20%;
   background-color: lightblue;
   height: 100vh;
 `;
 
 export const Main = styled.div`
-  width: 75%;
+  width: 80%;
   background-color: lightcoral;
   min-height: 100vh;
 `;
+
+export const Menu = styled.ul`
+  width: 100%;
+`;
+
+export const MenuItem = styled.li`
+  margin: 10px;
+  list-style: none;
+  padding: 10px;
+  box-shadow: var(--levelOneDropShadow);
+  border-radius: 2em;
+  border: 2px solid aliceblue;
+
+  :hover {
+    transition: var(--smoothTransition);
+    cursor: pointer;
+    box-shadow: var(--levelTwoDropShadow);
+  }
+`;
+
+export const BrowseMenu = styled.ul`
+  background-color: antiquewhite;
+`;
+
+export const BrowseMenuItem = styled.li`
+
+`;
+
