5e60658975635cf4096b7c95102e55d7c7ec56f1
castynet <riungemaina@gmail.coml>
2021-09-14 18:32:54 +0300

    components/user: added user items (temp)

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -11,10 +11,17 @@ export default function DashBoard() {
   return (
     <>
       <styled.Wrapper>
-        <styled.ProfileIcon
-          openUser={openUser}
-          onClick={() => expandUser()}
-        ></styled.ProfileIcon>
+        <styled.ProfileIcon openUser={openUser} onClick={() => expandUser()}>
+          <styled.Expanded openUser={openUser}>
+            <img src="" alt="User Profile" />
+            <p>my account</p>
+            <p>logout</p>
+            <p>edit profile</p>
+            <p>settings</p>
+            <p>dark theme toggle</p>
+            <p>payments</p>
+          </styled.Expanded>
+        </styled.ProfileIcon>
       </styled.Wrapper>
     </>
   );
