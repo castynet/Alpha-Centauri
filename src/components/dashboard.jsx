4a32d26aa0274bc845a31fa394b3eb644d466f6c
castynet <riungemaina@gmail.coml>
2021-09-27 21:20:57 +0300

    added icons

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,6 @@
 import * as styled from "./styles/dashboard.styles";
 import Search from "./search";
+import { Icons } from "./styles/icons.styles";
 
 export default function DashBoard() {
   return (
@@ -7,14 +8,26 @@ export default function DashBoard() {
       <styled.Wrapper>
         <styled.Sidebar>
           <styled.Menu>
-            <styled.MenuItem>Courses</styled.MenuItem>
-            <styled.MenuItem>Assignments</styled.MenuItem>
-            <styled.MenuItem>Tests</styled.MenuItem>
-            <styled.MenuItem>Community</styled.MenuItem>
+            <styled.MenuItem>
+              <Icons.Book size="18" title="View Courses" /> Courses
+            </styled.MenuItem>
+            <styled.MenuItem>
+              <Icons.Assignment size="18" title="View Courses" /> Assignments
+            </styled.MenuItem>
+            <styled.MenuItem>
+              <Icons.Tests size="18" title="View Courses" /> Tests
+            </styled.MenuItem>
+            <styled.MenuItem>
+              <Icons.Community size="18" title="View Courses" /> Community
+            </styled.MenuItem>
           </styled.Menu>
           <styled.BrowseMenu>
-            <styled.BrowseMenuItem>Messages</styled.BrowseMenuItem>
-            <styled.BrowseMenuItem>All Courses</styled.BrowseMenuItem>
+            <styled.BrowseMenuItem>
+              <Icons.Message size="18" title="View Courses" /> Messages
+            </styled.BrowseMenuItem>
+            <styled.BrowseMenuItem>
+              <Icons.AllCourses size="18" title="View Courses" /> All Courses
+            </styled.BrowseMenuItem>
           </styled.BrowseMenu>
         </styled.Sidebar>
         <styled.Main>
