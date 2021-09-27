fdb407c0dfc5f0c958a35c6f00a566e86a78db60
castynet <riungemaina@gmail.coml>
2021-09-27 21:20:44 +0300

    components/styles/icons.styles: created and addded icons

---
+++ src/components/styles/icons.styles.jsx
@@ -0,0 +1,17 @@
+import { Book } from "@styled-icons/fa-solid/Book";
+import { Building } from "@styled-icons/fa-solid/Building";
+import { PaperPlane } from "@styled-icons/fa-solid";
+import { Users } from "@styled-icons/fa-solid";
+import { CommentDots } from "@styled-icons/fa-solid";
+import { GlobeAfrica } from "@styled-icons/fa-solid";
+import { UserAstronaut } from "@styled-icons/fa-solid";
+
+export const Icons = {
+  Book: Book,
+  Assignment: Building,
+  Tests: PaperPlane,
+  Community: Users,
+  Message: CommentDots,
+  AllCourses: GlobeAfrica,
+  User: UserAstronaut,
+};
