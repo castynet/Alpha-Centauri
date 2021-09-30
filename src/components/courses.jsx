ebbad6d635e16e4215a32bf7d9e65fcff70f8f62
castynet <riungemaina@gmail.com>
2021-09-30 10:00:21 +0300

    added course items

--- src/components/courses.jsx
+++ src/components/courses.jsx
@@ -5,7 +5,14 @@ export default function Courses() {
     <>
       <styled.Wrapper>
         {[...Array(5)].map((e, i) => (
-          <styled.Course></styled.Course>
+          <styled.Course>
+            <p>Course Title</p>
+            <p>Course Description</p>
+            <p>Course Image</p>
+            <p>Progress | Rating</p>
+            <p>Continue Learning | Purchase</p>
+            <p>Number of enrolled students</p>
+          </styled.Course>
         ))}
       </styled.Wrapper>
     </>
