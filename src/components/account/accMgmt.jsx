83b9c9c0daf4048205e606d3a108c38bf64e46ee
castynet <riungemaina@gmail.com>
2021-10-13 11:09:08 +0300

    components/account/accMgmt: added input fields

--- src/components/account/accMgmt.jsx
+++ src/components/account/accMgmt.jsx
@@ -4,18 +4,39 @@ export default function AccountManagement() {
   return (
     <>
       <styled.Wrapper>
+        <styled.SectionTitle>Personal Details</styled.SectionTitle>
         <styled.Avatar src="" alt="Avatar" />
-        <p>first name</p>
-        <p>last name</p>
-        <p>username</p>
-        <p>email</p>
-        <p>phone number</p>
-        <p>email</p>
+        <br />
+        <styled.Label htmlFor="userName">Username: </styled.Label>
+        <styled.Input type="text" id="userName" />
+        <br />
+        <styled.Label htmlFor="firstName">First Name: </styled.Label>
+        <styled.Input type="text" id="firstName" />
+        <br />
+        <styled.Label htmlFor="lastName">Last Name: </styled.Label>
+        <styled.Input type="text" id="lastName" />
+        <br />
+        <styled.Label htmlFor="email">Email: </styled.Label>
+        <styled.Input type="text" id="email" />
+        <styled.SectionTitle>Linked Accounts Details</styled.SectionTitle>
+        <p>Google</p>
+        <styled.SectionTitle>Parent's Details</styled.SectionTitle>
+        <styled.Label htmlFor="parentName">Name: </styled.Label>
+        <styled.Input type="text" id="parentName" />
+        <br />
+        <styled.Label htmlFor="parentEmail">Email: </styled.Label>
+        <styled.Input type="text" id="parentEmail" />
+        <br />
+        <styled.Label htmlFor="phoneNumber">Phone: </styled.Label>
+        <styled.Input type="text" id="phoneNumber" />
+        <br />
+        <styled.Input type="checkbox" name="" id="" />
+        <styled.Label htmlFor="phoneNumberPayments">
+          Use the same number for payments:
+        </styled.Label>
+        {/* <p>no? enter phone number</p> */}
+        <styled.SectionTitle>Critical</styled.SectionTitle>
         <p>password</p>
-        <p>linked accounts</p>
-        <p>parent's phone number</p>
-        <p>checkbox | use same number for payments</p>
-        <p>no? enter phone number</p>
         <styled.DeleteBtn>Delete Account</styled.DeleteBtn>
       </styled.Wrapper>
     </>
