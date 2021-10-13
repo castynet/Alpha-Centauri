8bb5f35d27e6a53476de1bf2889a3cbeea4aa9c5
castynet <riungemaina@gmail.com>
2021-10-13 10:29:53 +0300

    components/general/alertBox.styles: added component wrapper

---
+++ src/components/general/alertBox.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 500px;
+  margin: 20px;
+  background-color: lightblue;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: var(--borderRadius);
+  padding: 20px;
+`;
