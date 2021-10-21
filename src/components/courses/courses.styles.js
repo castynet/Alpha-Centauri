e0db1c169d0f6f276be4a215dacab85f302d8486
castynet <riungemaina@gmail.com>
2021-10-21 15:30:59 +0300

    components/courses/courses.styles: styled title & course title img

--- src/components/courses/courses.styles.js
+++ src/components/courses/courses.styles.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin: 30px;
+  background-color: #88889980;
+  padding: 30px;
+`;
+
+export const CourseWrapper = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -12,7 +16,7 @@ export const Wrapper = styled.div`
 export const Course = styled.div`
   background-color: white;
   width: 350px;
-  height: 200px;
+  height: fit-content;
   box-shadow: var(--levelOneDropShadow);
   border-radius: var(--borderRadius);
   transition: var(--smoothTransition);
@@ -21,4 +25,21 @@ export const Course = styled.div`
   :hover {
     box-shadow: var(--levelTwoDropShadow);
   }
+`;
+
+export const Title = styled.h2`
+  background-color: white;
+  margin: 0 0 10px 0;
+  padding: 10px 10px 10px 40px;
+  border-radius: var(--borderRadius);
+  box-shadow: var(--levelOneDropShadow);
+  font-size: 1.3em;
+  text-decoration: underline;
+`;
+
+export const CourseTitle = styled.h3``;
+
+export const CourseImage = styled.img`
+  width: 100%;
+  border-radius: var(--borderRadius);
 `;
