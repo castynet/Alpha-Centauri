209d2181284007719d7692ca327c965eeec9d8fa
castynet <riungemaina@gmail.com>
2021-10-29 23:30:30 +0300

    components/community/community: added post & post wrapper

--- src/components/community/community.jsx
+++ src/components/community/community.jsx
@@ -4,7 +4,19 @@ export default function Community() {
   return (
     <>
       <styled.Wrapper>
-        <p>hello world</p>
+        <styled.Title>Community</styled.Title>
+        <styled.PostWrapper>
+          {[...Array(5)].map((e, i) => (
+            <styled.Post>
+              community post <br />
+              title <br />
+              type post download etc <br />
+              likes <br />
+              comments <br />
+              view <br />
+            </styled.Post>
+          ))}
+        </styled.PostWrapper>
       </styled.Wrapper>
     </>
   );
