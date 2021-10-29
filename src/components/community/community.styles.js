bfd8cb737e2beb5afcdd9835d69c761f2b3e0a87
castynet <riungemaina@gmail.com>
2021-10-29 23:30:56 +0300

    components/community/community.styles: styled titles, posts & post wrapper

--- src/components/community/community.styles.js
+++ src/components/community/community.styles.js
@@ -4,3 +4,31 @@ export const Wrapper = styled.div`
   background-color: #88889980;
   padding: 30px;
 `;
+
+export const Title = styled.h1`
+  background-color: white;
+  margin: 0 0 10px 0;
+  padding: 10px 10px 10px 40px;
+  border-radius: var(--borderRadius);
+  box-shadow: var(--levelOneDropShadow);
+  font-size: 1.3em;
+  text-decoration: underline;
+`;
+
+export const PostWrapper = styled.div`
+  margin: 30px 0 0 0;
+`;
+
+export const Post = styled.div`
+  background-color: white;
+  height: fit-content;
+  box-shadow: var(--levelOneDropShadow);
+  border-radius: var(--borderRadius);
+  transition: var(--smoothTransition);
+  padding: 10px;
+  margin: 15px 0;
+
+  :hover {
+    box-shadow: var(--levelTwoDropShadow);
+  }
+`;
