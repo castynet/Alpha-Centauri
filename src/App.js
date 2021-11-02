c939c5e0daebbda9d197e480dc1edb5b39cad1f1
castynet <riungemaina@gmail.com>
2021-11-03 00:07:54 +0300

    App: conditional rendering with user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import DashBoard from "./components/dashboard";
 import User from "./components/account/user";
+import { useApp } from "./components/context";
+import Auth from "./components/auth/auth";
 
 function App() {
+  const app = useApp();
+
+  const Display = () => {
+    if (app.user) {
+      return (
+        <>
+          <User />
+          <DashBoard />
+        </>
+      );
+    } else {
+      return <Auth />;
+    }
+  };
+
   return (
     <>
-      <User />
-      <DashBoard />
+      <Display />
     </>
   );
 }
