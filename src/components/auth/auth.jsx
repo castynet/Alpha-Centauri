e87febe8d9c614c63bcc6c3491ba1841d92b1c72
castynet <riungemaina@gmail.com>
2021-11-03 00:08:12 +0300

    components/auth/auth: call signin

---
+++ src/components/auth/auth.jsx
@@ -0,0 +1,29 @@
+import * as styled from "./auth.styles";
+import { Icons } from "../icons.styles";
+import { useApp } from "../context";
+
+export default function Auth() {
+  const app = useApp();
+
+  return (
+    <>
+      <styled.Wrapper>
+        <styled.Title>Sign in to Alpha Centauri</styled.Title>
+        <styled.ButtonWrapper>
+          <styled.Button onClick={() => app.signIn()}>
+            <Icons.Google size="20" />
+            <styled.BtnText>Sign in with Google</styled.BtnText>
+          </styled.Button>
+          <styled.Button>
+            <Icons.Login size="20" />
+            <styled.BtnText>Login with your Email</styled.BtnText>
+          </styled.Button>
+          <styled.Button>
+            <Icons.Register size="20" />
+            <styled.BtnText>Register with your Email</styled.BtnText>
+          </styled.Button>
+        </styled.ButtonWrapper>
+      </styled.Wrapper>
+    </>
+  );
+}
