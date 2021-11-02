a30bfd099ade1413e1fc7d1f9aebb2c8d44a2c78
castynet <riungemaina@gmail.com>
2021-11-03 00:07:37 +0300

    components/context: added signin with google

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -1,15 +1,50 @@
 import React, { useContext, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { firebaseConfig } from "./firebase";
 
 const Context = React.createContext();
+
+// initialize Firebase App
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Firebase Auth
+const provider = new GoogleAuthProvider();
+const auth = getAuth(firebaseApp);
 
 export function useApp() {
   return useContext(Context);
 }
 
 export const ContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [signedIn, setSignedIn] = useState(false);
   const [view, setView] = useState("Courses");
+  const [theme, setTheme] = useState("light");
+
+  async function signIn() {
+    const result = await signInWithPopup(auth, provider);
+    const credential = await GoogleAuthProvider.credentialFromResult(result);
+    setToken(credential.accessToken);
+    setUser(result.user);
+  }
 
   return (
-    <Context.Provider value={{ view, setView }}>{children}</Context.Provider>
+    <Context.Provider
+      value={{
+        view,
+        setView,
+        theme,
+        setTheme,
+        signedIn,
+        setSignedIn,
+        user,
+        token,
+        signIn,
+      }}
+    >
+      {children}
+    </Context.Provider>
   );
 };
