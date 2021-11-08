0ecda88d6931dc9f6c531133d6bec8a24fe035c2
castynet <riungemaina@gmail.com>
2021-11-09 00:02:17 +0300

    components/courses/courses: added view all courses

--- src/components/courses/courses.jsx
+++ src/components/courses/courses.jsx
@@ -1,18 +1,45 @@
 import * as styled from "./courses.styles";
+import { useApp } from "../context";
 
 export default function Courses() {
+  const app = useApp();
+
+  const addCommaToNumber = (number) => {
+    return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  const handleClick = () => {
+    app.view === "My Courses"
+      ? app.setView("All Courses")
+      : app.setView("My Courses");
+  };
+
   return (
     <>
       <styled.Wrapper>
-        <styled.Title> All Courses</styled.Title>
+        <styled.Title>
+          <h2>{app.view === "All Courses" ? "All Courses" : "My Courses"}</h2>
+          <styled.AltB view={app.view} onClick={() => handleClick()}>
+            {app.view === "All Courses" ? "My Courses" : "All Courses"}
+          </styled.AltB>
+        </styled.Title>
         <styled.CourseWrapper>
-          {[...Array(5)].map((e, i) => (
-            <styled.Course>
-              <styled.CourseImage
-                src="https://digikids.co.ke/wp-content/uploads/2019/05/minecraft.png"
-                alt="courseImage"
-              />
-              <styled.CourseTitle>Course Title</styled.CourseTitle>
+          {app.courses.map((course) => (
+            <styled.Course key={course.title}>
+              <styled.ImageCrop>
+                <styled.CourseImage
+                  src={course.displayImg}
+                  alt={`Course Image - ${course.title}`}
+                />
+              </styled.ImageCrop>
+
+              <styled.CourseTitle>{course.title}</styled.CourseTitle>
+              <styled.CourseInfo>
+                <styled.CourseLang>{course.lang}</styled.CourseLang>
+                <styled.Cost>
+                  Cost: KES {addCommaToNumber(course.cost)}
+                </styled.Cost>
+              </styled.CourseInfo>
             </styled.Course>
           ))}
         </styled.CourseWrapper>
