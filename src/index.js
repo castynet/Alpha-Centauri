8ac833aa875402605a4258a02008a6c801c4c176
castynet <riungemaina@gmail.com>
2021-11-13 01:27:29 +0300

    index: added browser router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ContextProvider } from "./components/context";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
