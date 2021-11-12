a6507c1b910ae0aaf5a3b8059a8589f743d92c4a
castynet <riungemaina@gmail.com>
2021-11-13 01:29:05 +0300

    components/courses/singleCourse.styles: styled wrapper

--- src/components/courses/singleCourse.styles.js
+++ src/components/courses/singleCourse.styles.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   margin: 30px;
   padding: 20px;
+  background-color: white;
+  border-radius: var(--borderRadius);
+  box-shadow: var(--levelOneDropShadow);
+  transition: var(--smoothTransition);
 `;
