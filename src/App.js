a00f5d1ae7e2467a830aacd3fe04825df2349fc7
castynet <riungemaina@gmail.com>
2021-11-15 16:39:48 +0300

    App: show alert box if it should be shown

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import DashBoard from "./components/dashboard";
 import User from "./components/account/user";
 import { useApp } from "./components/context";
 import Auth from "./components/auth/auth";
+import AlertBox from "./components/general/alertBox";
 
 function App() {
   const app = useApp();
@@ -21,6 +22,7 @@ function App() {
 
   return (
     <>
+      {app.popup.open ? <AlertBox /> : ""}
       <Display />
     </>
   );
