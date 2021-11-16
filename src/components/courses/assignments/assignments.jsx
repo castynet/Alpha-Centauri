8ed78d430e62fb5dac5656e1c401a37208aecab0
castynet <riungemaina@gmail.com>
2021-11-17 02:33:07 +0300

    added markup & logic

--- src/components/courses/assignments/assignments.jsx
+++ src/components/courses/assignments/assignments.jsx
@@ -1,23 +1,79 @@
+import { useEffect } from "react";
 import * as styled from "./assignments.styles";
+import { useApp } from "../../context";
+import Loading from "../../general/loading";
+import { Link } from "react-router-dom";
+import { Icons } from "../../icons.styles";
 
 export default function Assignments() {
+  const app = useApp();
+
+  useEffect(() => {
+    if (app.assignment.length === 0) {
+      app.getAssignments();
+    }
+  }, [app]);
+
+  const Ldng = (param) => {
+    return param.loaded > 3000 ? (
+      <p>No Assignments</p>
+    ) : (
+      <>
+        <Loading />
+      </>
+    );
+  };
+
+  const getDate = (date) => {
+    var dt = new Date();
+    dt.setTime(date * 1000);
+    return dt.toDateString();
+  };
+
   return (
     <>
       <styled.Wrapper>
         <styled.Title>Assignments</styled.Title>
-        <styled.AssignmentWrapper>
-          {[...Array(5)].map((e, i) => (
-            <styled.Assignment>
-              Assignment Title
-              <br /> Due Date
-              <br /> Done?
-              <br /> Grade?
-              <br /> Course
-              <br /> Est Duration
-              <br /> View
-            </styled.Assignment>
-          ))}
-        </styled.AssignmentWrapper>
+        {app.assignment.length === 0 ? (
+          <Ldng
+            loaded={setTimeout(() => {
+              return true;
+            }, 3000)}
+          />
+        ) : (
+          <styled.AssignmentWrapper>
+            {app.assignment.map((e) => (
+              <Link
+                style={{
+                  display: "block",
+                  textDecoration: "none",
+                  color: "inherit",
+                }}
+                to={`/assignments/${e.slug}`}
+                key={e.slug}
+              >
+                <styled.Assignment>
+                  <div>
+                    <Icons.ArrowRight size="30" />
+                  </div>
+                  <h3>{e.title}</h3>
+                  <p>
+                    <span>Due Date</span>:&nbsp;&nbsp;
+                    {getDate(e.dueDate.seconds)}
+                  </p>
+                  <p>
+                    <span>Grade</span>:&nbsp;&nbsp;
+                    {e.grade ? e.grade : "Not available"}
+                  </p>
+                  <p>
+                    <span>Estimated Duration</span>:&nbsp;&nbsp;
+                    {e.estDuration / 60} Hours
+                  </p>
+                </styled.Assignment>
+              </Link>
+            ))}
+          </styled.AssignmentWrapper>
+        )}
       </styled.Wrapper>
     </>
   );
--- src/components/courses/quizzes/tests.jsx
+++ src/components/courses/quizzes/tests.jsx
@@ -1,23 +1,82 @@
 import * as styled from "./tests.styles";
+import { useEffect } from "react";
+import { useApp } from "../../context";
+import Loading from "../../general/loading";
+import { Link } from "react-router-dom";
+import { Icons } from "../../icons.styles";
 
 export default function Tests() {
+  const app = useApp();
+
+  useEffect(() => {
+    if (app.tests.length === 0) {
+      app.getTests();
+    }
+  }, [app]);
+
+  const Ldng = (param) => {
+    return param.loaded > 3000 ? (
+      <p>No Tests</p>
+    ) : (
+      <>
+        <Loading />
+      </>
+    );
+  };
+
+  const getDate = (date) => {
+    var dt = new Date();
+    dt.setTime(date * 1000);
+    return dt.toDateString();
+  };
+
+  const convertTime = (n) =>
+    `0${(n / 60) ^ 0}`.slice(-2) + ":" + ("0" + (n % 60)).slice(-2);
+
   return (
     <>
       <styled.Wrapper>
         <styled.Title>Tests</styled.Title>
-        <styled.TestWrapper>
-          {[...Array(10)].map((e, i) => (
-            <styled.Test>
-              Test Title
-              <br /> Due Date
-              <br /> Done?
-              <br /> Grade?
-              <br /> Course
-              <br /> Est Duration
-              <br /> View
-            </styled.Test>
-          ))}
-        </styled.TestWrapper>
+        {app.tests.length === 0 ? (
+          <Ldng
+            loaded={setTimeout(() => {
+              return true;
+            }, 3000)}
+          />
+        ) : (
+          <styled.TestWrapper>
+            {app.tests.map((e) => (
+              <Link
+                style={{
+                  display: "block",
+                  textDecoration: "none",
+                  color: "inherit",
+                }}
+                to={`/tests/${e.slug}`}
+                key={e.slug}
+              >
+                <styled.Test>
+                  <h3>{e.title}</h3>
+                  <p>
+                    <span>Due Date</span>:&nbsp;&nbsp;
+                    {getDate(e.dueDate.seconds)}
+                  </p>
+                  <p>
+                    <span>Grade</span>:&nbsp;&nbsp;
+                    {e.grade ? e.grade : "Not available"}
+                  </p>
+                  <p>
+                    <span>Est Duration</span>:&nbsp;&nbsp;
+                    {convertTime(e.estDuration)}
+                  </p>
+                  <div>
+                    <Icons.ArrowDown size="30" />
+                  </div>
+                </styled.Test>
+              </Link>
+            ))}
+          </styled.TestWrapper>
+        )}
       </styled.Wrapper>
     </>
   );
